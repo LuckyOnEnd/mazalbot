e391d8701f8623c19e65b646df5ab0f1c4a621b3
LuckyOnEnd <58709337+LuckyOnEnd@users.noreply.github.com>
2024-09-17 08:36:58 +0200

    Update deployment_script.sh

--- release_v1-2-2/deployment_script.sh
+++ release_v1-2-2/deployment_script.sh
@@ -1,4 +1,9 @@
 git pull
 sudo systemctl stop nginx
-sudo docker-compose up --build -d
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
+docker-compose down
+docker-compose up --build -d
 sudo systemctl restart nginx
+
